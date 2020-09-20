d908a6a8341aea00a20607963e71b49ed1e3b7ae
okuoku <mjt@cltn.org>
2020-09-20 19:55:27 +0900

    9p: add stub

--- emmain.c
+++ emmain.c
@@ -49,64 +49,69 @@ chr_read_data(void* bogus, uint8_t *buf, int len){
     return 0;
 }
 
-void* bios_ptr;
-int bios_size;
-
-void* kernel_ptr;
-int kernel_size;
-
-uintptr_t 
-ememu_configure(int req, uintptr_t param0, uintptr_t param1){
-    uintptr_t r = 0;
-    switch(req){
-        case 1: /* malloc */
-            r = (uintptr_t)malloc(param0);
-            break;
-        case 100: /* SET_BIOS */
-            bios_ptr = (void*)param0;
-            bios_size = param1;
-            break;
-        case 101: /* SET_KERNEL */
-            kernel_ptr = (void*)param0;
-            kernel_size = param1;
-            break;
-        default:
-            fprintf(stderr, "Invalid argument.\n");
-            abort();
-            break;
-    }
-    printf("CONFIG %d: %lx %lx => %lx\n", req, param0, param1, r);
-    return r;
-}
+/* Stubs */
 
 static void
 myfs_end(FSDevice *s){
+    /* UNUSED */
 }
 
 static void
 myfs_delete(FSDevice* s, FSFile* f){
+    /* UNUSED? */
 }
 
 static void
 myfs_statfs(FSDevice* fs, FSStatFS* st){
+    st->f_bsize = 1024;
+    st->f_blocks = 999999;
+    st->f_bfree = 999999;
+    st->f_bavail = 999999;
+    st->f_files = 999999;
+    st->f_ffree = 999999;
 }
+
+/* System OPs */
+
+/* fsattach: 200 */
+struct FSFile {
+    int dummy;
+};
+typedef struct FSFile FSFile;
+static int (*fsattach)(uint32_t ctx,
+                       uint32_t fsfile, uint32_t uid, 
+                       uint32_t str_uname, uint32_t str_aname,
+                       uint32_t out_addr_qid_type /* 32bit */,
+                       uint32_t out_addr_qid_version /* 32bit */,
+                       uint32_t out_addr_qid_path /* 64bit */);
+
 
 static int
 myfs_attach(FSDevice *fs, FSFile **pf, FSQID *qid, uint32_t uid,
                      const char *uname, const char *aname){
-    return -P9_EIO;
-}
-
-static int
-myfs_walk(FSDevice *fs, FSFile **pf, FSQID *qids,
-                   FSFile *f, int n, char **names){
-    return -P9_EIO;
-}
-
-static int
-myfs_mkdir(FSDevice *fs, FSQID *qid, FSFile *f,
-                    const char *name, uint32_t mode, uint32_t gid){
-    return -P9_EIO;
+    FSFile* ident;
+    int r;
+    uint32_t type;
+    uint32_t version;
+    uint64_t path;
+    ident = malloc(sizeof(FSFile));
+    r = fsattach((uint32_t)(uintptr_t)fs,
+                 (uint32_t)(uintptr_t)ident, uid,
+                 (uint32_t)(uintptr_t)uname,
+                 (uint32_t)(uintptr_t)aname,
+                 (uint32_t)(uintptr_t)&type,
+                 (uint32_t)(uintptr_t)&version,
+                 (uint32_t)(uintptr_t)&path);
+    if(r){
+        free(ident);
+        return -r;
+    }else{
+        *pf = ident;
+        qid->type = type;
+        qid->version = version;
+        qid->path = path;
+        return 0;
+    }
 }
 
 static int
@@ -115,27 +120,20 @@ myfs_open(FSDevice *fs, FSQID *qid, FSFile *f, uint32_t flags,
     return -P9_EIO;
 }
 
-static int
-myfs_create(FSDevice *fs, FSQID *qid, FSFile *f, const char *name,
-                     uint32_t flags, uint32_t mode, uint32_t gid){
-    return -P9_EIO;
+static void
+myfs_close(FSDevice *fs, FSFile *f){
 }
 
+/* Tree Query */
+
+static int
+myfs_walk(FSDevice *fs, FSFile **pf, FSQID *qids,
+                   FSFile *f, int n, char **names){
+    return -P9_EIO;
+}
 static int
 myfs_stat(FSDevice *fs, FSFile *f, FSStat *st){
     return -P9_EIO;
-}
-
-static int
-myfs_setattr(FSDevice *fs, FSFile *f, uint32_t mask,
-                      uint32_t mode, uint32_t uid, uint32_t gid,
-                      uint64_t size, uint64_t atime_sec, uint64_t atime_nsec,
-                      uint64_t mtime_sec, uint64_t mtime_nsec){
-    return -P9_EIO;
-}
-
-static void
-myfs_close(FSDevice *fs, FSFile *f){
 }
 
 static int
@@ -144,33 +142,11 @@ myfs_readdir(FSDevice *fs, FSFile *f, uint64_t offset,
     return -P9_EIO;
 }
 
+/* Read */
+
 static int
 myfs_read(FSDevice *fs, FSFile *f, uint64_t offset,
             uint8_t *buf, int count){
-    return -P9_EIO;
-}
-
-static int
-myfs_write(FSDevice *fs, FSFile *f, uint64_t offset,
-             const uint8_t *buf, int count){
-    return -P9_EIO;
-}
-
-static int
-myfs_link(FSDevice *fs, FSFile *df, FSFile *f, const char *name){
-    return -P9_EIO;
-}
-
-static int
-myfs_symlink(FSDevice *fs, FSQID *qid,
-                      FSFile *f, const char *name, const char *symgt, uint32_t gid){
-    return -P9_EIO;
-}
-
-static int
-myfs_mknod(FSDevice *fs, FSQID *qid,
-                    FSFile *f, const char *name, uint32_t mode, uint32_t major,
-                    uint32_t minor, uint32_t gid){
     return -P9_EIO;
 }
 
@@ -179,14 +155,70 @@ myfs_readlink(FSDevice *fs, char *buf, int buf_size, FSFile *f){
     return -P9_EIO;
 }
 
+/* Tree Write */
+
+static int
+myfs_mkdir(FSDevice *fs, FSQID *qid, FSFile *f,
+                    const char *name, uint32_t mode, uint32_t gid){
+    /* WRITE */
+    return -P9_EIO;
+}
+
+static int
+myfs_create(FSDevice *fs, FSQID *qid, FSFile *f, const char *name,
+                     uint32_t flags, uint32_t mode, uint32_t gid){
+    /* WRITE */
+    return -P9_EIO;
+}
+
+static int
+myfs_setattr(FSDevice *fs, FSFile *f, uint32_t mask,
+                      uint32_t mode, uint32_t uid, uint32_t gid,
+                      uint64_t size, uint64_t atime_sec, uint64_t atime_nsec,
+                      uint64_t mtime_sec, uint64_t mtime_nsec){
+    /* WRITE */
+    return -P9_EIO;
+}
+
+static int
+myfs_link(FSDevice *fs, FSFile *df, FSFile *f, const char *name){
+    /* WRITE */
+    return -P9_EIO;
+}
+
+static int
+myfs_mknod(FSDevice *fs, FSQID *qid,
+                    FSFile *f, const char *name, uint32_t mode, uint32_t major,
+                    uint32_t minor, uint32_t gid){
+    /* WRITE */
+    return -P9_EIO;
+}
+
 static int
 myfs_renameat(FSDevice *fs, FSFile *f, const char *name,
                        FSFile *new_f, const char *new_name){
+    /* WRITE */
     return -P9_EIO;
 }
 
 static int
 myfs_unlinkat(FSDevice *fs, FSFile *f, const char *name){
+    /* WRITE */
+    return -P9_EIO;
+}
+
+/* Write */
+static int
+myfs_write(FSDevice *fs, FSFile *f, uint64_t offset,
+             const uint8_t *buf, int count){
+    /* WRITE */
+    return -P9_EIO;
+}
+
+static int
+myfs_symlink(FSDevice *fs, FSQID *qid,
+                      FSFile *f, const char *name, const char *symgt, uint32_t gid){
+    /* WRITE */
     return -P9_EIO;
 }
 
@@ -227,6 +259,47 @@ FSDevice myfs = {
     .fs_lock = myfs_lock,
     .fs_getlock = myfs_getlock
 };
+
+void* bios_ptr;
+int bios_size;
+
+void* kernel_ptr;
+int kernel_size;
+
+uintptr_t 
+ememu_configure(int req, uintptr_t param0, uintptr_t param1){
+    uintptr_t r = 0;
+    switch(req){
+        case 1: /* malloc */
+            r = (uintptr_t)malloc(param0);
+            break;
+        case 100: /* SET_BIOS */
+            bios_ptr = (void*)param0;
+            bios_size = param1;
+            break;
+        case 101: /* SET_KERNEL */
+            kernel_ptr = (void*)param0;
+            kernel_size = param1;
+            break;
+        case 102: /* SET_FSOPS */
+            switch(param0){
+                case 200:
+                    fsattach = (void*)param1;
+                    break;
+                default:
+                    fprintf(stderr, "Unknown fsop");
+                    abort();
+                    break;
+            }
+            break;
+        default:
+            fprintf(stderr, "Invalid argument.\n");
+            abort();
+            break;
+    }
+    printf("CONFIG %d: %lx %lx => %lx\n", req, param0, param1, r);
+    return r;
+}
 
 
 
--- index.js
+++ index.js
@@ -5,8 +5,37 @@ const emu = require("./emu_tinyemu.js");
 const kernel = fs.readFileSync("kernel");
 const bios = fs.readFileSync("bbl32.bin");
 
+
+function addfsops(module){
+    // FIXME: Why not the_emu._ememu_configure ??
+    const ememu_configure =
+        module.cwrap("ememu_configure", "number", ["number", "number"]);
+
+    function fill_qid(addr_type, addr_version, addr_path,
+                      type, version, path){
+        module.setValue(addr_type, type, "i32"); // our own protocol
+        module.setValue(addr_version, version, "i32");
+        module.setValue(addr_path, path, "i64");
+    }
+    function readpath(addr){
+        return module.UTF8ToString(addr, 4096);
+    }
+
+    function fsattach(ctx, fsfile, uid,
+                      str_uname, str_aname,
+                      addr_qid_type, addr_qid_version, addr_qid_path){
+        const uname = readpath(str_uname);
+        const aname = readpath(str_aname);
+        console.log("Attach",uid,uname,aname);
+        return 5;
+    }
+
+    ememu_configure(102, 200, module.addFunction(fsattach, "iiiiiiiii"));
+
+}
+
 async function start(){
-    the_emu = await emu();
+    const the_emu = await emu();
 
     // FIXME: Why not the_emu._ememu_configure ??
     const ememu_configure =
@@ -25,6 +54,7 @@ async function start(){
 
     ememu_configure(100, bios_addr, bios_len);
     ememu_configure(101, kernel_addr, kernel_len);
+    addfsops(the_emu);
 
     ememu_start();
 }
